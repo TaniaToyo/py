aa93cebba0917ec809eaa394837491294f71fcc2
Tania Toyo <taniatoyo03@gmail.com>
2020-12-07 20:25:24 +0530

    Opencv-chapter 1

--- Opencv_chap1.py
+++ Opencv_chap1.py
@@ -6,3 +6,16 @@ cv2.waitKey(0)
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Gray', imgGray)
 cv2.waitKey(0)
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 500)
+cap.set(10, 100)
+while True:
+    success,img=cap.read()
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+
+
